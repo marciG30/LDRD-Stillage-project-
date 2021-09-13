fad9a8e7e1c2c2ec80b643bcb96b885d9aee8fe7
marciG30 <63817766+marciG30@users.noreply.github.com>
2021-09-13 16:41:26 -0700

    Changed labels to new ones

--- code/BioscreenC_08_24_2021_Marci.R
+++ code/BioscreenC_08_24_2021_Marci.R
@@ -1,6 +1,6 @@
 library(tidyverse)  
 library(lubridate)
-BC <- read.csv(file = "data/BioscreenExperiment_shewy_8_7_21.csv", header = TRUE, sep = ",", stringsAsFactors=FALSE)
+BC <- read.csv(file = "data_bioscreen/21_08_27_shewy_variant_BioscreenExperiment.csv", header = TRUE, sep = ",", stringsAsFactors=FALSE)
 BC2 <- gather(BC,key = "Well","OD", 2:201)
 BC2$Time <- hms(BC2$Time )
 BC2 <- BC2 %>% mutate(HOURS = hour(BC2$Time) + minute(BC2$Time) / 60 + second(BC2$Time) / 360)
@@ -11,25 +11,23 @@ BC2_new <- BC2 %>% mutate(Well2 = str_remove_all(Well, "Well."))
 # If the period was still in, the numbers would be parsed as 0.101, etc.
 # create a new column with conditions, using "case_when"
 # syntax for case_when is: CONDITION1 ~ a, CONDITION2 ~ b, etc.
-BC2_new <- BC2_new %>%    mutate(Media_Type = case_when(Well2 >= 101 & Well2 <= 110 ~ "A.niger",
-                                                        Well2 >= 111 & Well2 <= 120 ~ "A.niger_Blank",
-                                                        Well2 >= 121 & Well2 <= 130 ~ "A.niger_wt",
-                                                        Well2 >= 131 & Well2 <= 140 ~ "A.niger_wt_Blank",
-                                                        Well2 >= 141 & Well2 <= 150 ~ "Lacto D. O2 limit",
-                                                        Well2 >= 151 & Well2 <= 160 ~ "Lacto D. O2 limit_Blank",
-                                                        Well2 >= 161 & Well2 <= 170 ~ "Yarrowin Lipo",
-                                                        Well2 >= 171 & Well2 <= 180 ~ "Yarrowin Lipo_Blank",
-                                                        Well2 >= 181 & Well2 <= 190 ~ "NanoWire",
+BC2_new <- BC2_new %>%    mutate(Media_Type = case_when(Well2 >= 101 & Well2 <= 110 ~ "sodium sulfate",
+                                                        Well2 >= 111 & Well2 <= 120 ~ "sodium sulfate_blank",
+                                                        Well2 >= 121 & Well2 <= 130 ~ "amonium nitrate",
+                                                        Well2 >= 131 & Well2 <= 140 ~ "amonium nitrate_blank",
+                                                        Well2 >= 141 & Well2 <= 150 ~ "dipotasium phosphate",
+                                                        Well2 >= 151 & Well2 <= 160 ~ "dipotasium phosphate_Blank",
+                                                        Well2 >= 161 & Well2 <= 170 ~ "ferric citrate",
+                                                        Well2 >= 171 & Well2 <= 180 ~ "ferric citrate _Blank",
                                                         Well2 >= 191 & Well2 <= 200 ~ "H20",
-                                                        Well2 >= 201 & Well2 <= 210 ~ "lacto D.aerobic",
-                                                        Well2 >= 211 & Well2 <= 220 ~ "lacto D.aerobic_Blank",
-                                                        Well2 >= 221 & Well2 <= 230 ~ "Fodder yeast",
-                                                        Well2 >= 231 & Well2 <= 240 ~ "Fodder yeast_blank",
-                                                        Well2 >= 241 & Well2 <= 250 ~ "lacto D. anaerobic",
-                                                        Well2 >= 251 & Well2 <= 260 ~ "lacto D. anaerobic_Blank",
-                                                        Well2 >= 261 & Well2 <= 270 ~ "Nanowire",
-                                                        Well2 >= 271 & Well2 <= 280 ~ "Nanowire_blank",
-                                                        Well2 >= 281 & Well2 <= 290~ "H20",
+                                                        Well2 >= 201 & Well2 <= 210 ~ "formate",
+                                                        Well2 >= 211 & Well2 <= 220 ~ "formate_Blank",
+                                                        Well2 >= 221 & Well2 <= 230 ~ "pyruvate",
+                                                        Well2 >= 231 & Well2 <= 240 ~ "pyruvate_Blank",
+                                                        Well2 >= 241 & Well2 <= 250 ~ "nothing added",
+                                                        Well2 >= 251 & Well2 <= 260 ~ "nothing added _Blank",
+                                                        Well2 >= 261 & Well2 <= 270 ~ "everything added",
+                                                        Well2 >= 271 & Well2 <= 280 ~ "everything added_blank",
                                                         Well2 >= 291 & Well2 <= 300 ~ "H20",))
 rm(BC)
 rm(BC2)
@@ -59,18 +57,19 @@ BC2_new_kp =
   filter(!is.na(Media_name)) %>% 
   replace(is.na(.), 0) %>% 
   mutate(OD_corr = sample - blank,
-         OD_corr = if_else(Media_name == "Water", abs(OD_corr), OD_corr)) 
-  
+         OD_corr = if_else(Media_name == "Water", abs(OD_corr), OD_corr))+
+  mutate(OD_corr = if_else(OD_corr < 0, 0, OD_corr)
+
 BC2_new_kp %>% 
   ggplot(aes(x=HOURS,y=OD_corr))+
-  geom_point(size = 0.1)+
-  #ylim(0,1.5)+
-  ggtitle("Shewanella W3-18-1 Growth On Various Media using Bioscreen C") +
+  geom_point(size = 0.5)+
+  ylim(0,1.5)+
+  ggtitle("Shewanella W3-18-1 Growth nutriant limited On A. niger stillage") +
   xlab("Time (hours)") +
   ylab("Optical Density @ 600 nm")+
   facet_wrap(~Media_name)
 
-ggsave("output/shewanella_OD_stillages_21_08_07_combined.tiff")
+ggsave("output/Shewanella W3-18-1 Growth nutriant limited_21_08_27_combined.tiff")
 
 BC2_new_kp %>% ggplot(aes(x=HOURS, y=OD_corr, color = Media_name))+geom_point()+xlab("Time (Hours)")+ylab("Optical Density @ 600 nm")
-ggsave("shewanella_OD_stillages_21_05_10_combined_1graph.tiff")
+ggsave("Shewanella W3-18-1 Growth nutriant limited_21_08_27_combined_1graph.tiff")
